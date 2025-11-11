a6f52f699d9d418eda409ed6dc864922499c8f25
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-11-11 12:34:26 +0300

    update-home

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,20 @@
 'use client'
-export
+
+import { div } from "framer-motion/client"
+
+export default function Home(){
+  return(
+    <div className="grid grid-cols-2 ">
+      <div>
+        <h1 className="uppercase font-bold pt-20 px-29 text-orange-400">best destination around the world</h1>
+        <pre  className="px-29 text-6xl text-blue-950 font-black pt-3">Travel,enjoy </pre>
+          <pre className="px-29 text-6xl text-blue-950 font-black pt-3">and live anew </pre>
+         <pre className="px-29 text-6xl text-blue-950 font-black pt-3">and full life.</pre> 
+      </div>
+      <div>
+        kjbvhkbvsjk
+      </div>
+
+    </div>
+  )
+}
