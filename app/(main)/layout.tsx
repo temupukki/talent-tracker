77ec7cf9e2226e3e6efac8f82cc9eded3d45584d
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-11-15 17:40:34 +0300

    update-layout-structure

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div>
+    <NavBar/>
+    {children}
+    <Footer/>
+    </div>;
+
+}
