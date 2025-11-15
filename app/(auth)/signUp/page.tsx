a9bc5efaf30746fe0ae36518b082fd539fcb1226
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-11-15 15:59:15 +0300

    add-signup-page

---
+++ app/(auth)/signUp/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { motion } from "framer-motion";
+export default function signUp() {
+  return (
+    <div className="grid grid-cols-2 bg-[url('/addis.jfif')] bg-cover bg-center h-screen ">
+      <div className="ml-90 mt-30">
+        <h1 className="text-white text-4xl font-bold mb-46  ">
+          Certi<span className="text-orange-600 font-black">F</span>low.
+        </h1>
+        <h1 className="text-3xl font-semibold text-white">
+          Build the Future...
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+        <p>
+          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </p>
+        <p>Ut enim ad minim veniam, quis nostrud. </p>
+      </div>
+      <div>kjsdfnkjsfnk</div>
+    </div>
+  );
+}
