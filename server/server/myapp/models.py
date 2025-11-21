ac29b98df4f59a5e141688649be242800b035edf
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-11-21 19:47:04 +0300

    update

--- server/server/myapp/models.py
+++ server/server/myapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Job(models.Model):
-    name = models.CharField(180)
-    location = models.CharField(189)
-    company = models.CharField(90)
+    name = models.CharField( max_length=180)
+    location = models.CharField(max_length=190)
+    company = models.CharField(max_length=190)
     salary = models.IntegerField()
     stock = models.IntegerField()
