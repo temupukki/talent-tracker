4d96344f5728abb756cf686d4d7487021557989d
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-11-21 20:11:27 +0300

    deleting-the-server

--- server/server/myapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Job(models.Model):
-    name = models.CharField( max_length=180)
-    location = models.CharField(max_length=190)
-    company = models.CharField(max_length=190)
-    salary = models.IntegerField()
-    stock = models.IntegerField()
